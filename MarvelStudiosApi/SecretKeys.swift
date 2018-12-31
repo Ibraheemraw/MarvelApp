15a176a58c16e0ed9e21667ef4240c7ea903dc49
ibraheemraw <ibraheemrawlinson@pursuit.org>
2018-12-31 13:14:21 -0500

    created a charcater model class to fetch the list of characters and created a SecretKeys File to store my keys

---
+++ MarvelStudiosApi/SecretKeys.swift
@@ -0,0 +1,15 @@
+//
+//  SecretKeys.swift
+//  MarvelStudiosApi
+//
+//  Created by Ibraheem rawlinson on 12/31/18.
+//  Copyright © 2018 Ibraheem rawlinson. All rights reserved.
+//
+
+import Foundation
+
+class SecretKeys {
+    static let ts = "1"
+    static let apiKey = "19bff63c692ceeab404b3b2d890731c8"
+    static let hash = "d44f547dd0d7a1d51b358e0d6d876916"
+}
